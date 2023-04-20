b898660ee9bb141c4e85130617bfae49513e019e
Chukwuma Precious <Chukwumaprince08@gmail.com>
2023-04-20 23:58:00 +0400

    project initialization
    initial project setup

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
+namespace RoomBookingApp.Core
+{
+	public class RoomBookingRequestProcessorTest
+	{
+		[Fact]
+		public void Should_Return_Room_Booking_Response_With_Request_Values()
+		{
+			// Arrange
+			var request = new RoomBookingRequest
+			{
+				FullName = "Test Name",
+				Email = "test@request.com",
+				Date = new DateTime(2023, 10, 20)
+			};
+
+			var processor = new RoomBookingRequestProcessor();
+
+
+			//Act
+			RoomBookingResult result = processor.BookRoom(request);
+
+
+			// Assert
+			Assert.NotNull(result);
+			Assert.Equal(request.FullName, result.FullName);
+			Assert.Equal(request.Email, result.Email);
+			Assert.Equal(request.Date, result.Date);
+
+
+			result.ShouldNotBeNull();
+			result.FullName.ShouldBe(result.FullName);
+            result.Email.ShouldBe(result.Email);
+            result.Date.ShouldBe(result.Date);
+
+        }
+	}
+}
+
---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            return new RoomBookingResult
+            {
+               FullName = bookingRequest.FullName,
+               Date = bookingRequest.Date,
+               Email = bookingRequest.Email
+            };
+        }
+    }
+}
