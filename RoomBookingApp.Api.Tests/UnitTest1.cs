0a2770dfc8572650978feae2ece333e2d2b5e94a
Chukwuma Precious <Chukwumaprince08@gmail.com>
2023-04-23 01:05:14 +0400

    project setups for unit testing UI/API

---
+++ RoomBookingApp.Api.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace RoomBookingApp.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
